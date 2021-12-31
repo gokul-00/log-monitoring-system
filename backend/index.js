e670f225304ee755e58f0fe64783262080e0c692
gokul-00 <ygokul1702@gmail.com>
2021-12-31 23:35:17 +0530

    remove unwanted files

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const db = require("./db");
-const port = process.env.PORT || 3001;
-
-db.connect().then(() => {
-	console.log("Connected to MongoDB: " + db.url);
-
-	app.listen(port, () => console.log(`Server started on port ${port}...`));
-});
